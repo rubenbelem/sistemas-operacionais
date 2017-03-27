654501d23523c07ce05e72c134b3b7cf04fb3659
Rúben Belém <rubenbelem@gmail.com>
2017-03-26 22:48:54 -0400

    Commit antes de inciar a programação da classe Scheduler

--- Trabalho 1/sjf.cpp
+++ Trabalho 1/sjf.cpp
@@ -29,16 +29,15 @@ class CompareProcess {
 };
 
 void execute_step(queue<Process>& ex_queue) {
-
     if (!ex_queue.empty()) {
         Process &p = ex_queue.front();
         p.process_time--;
 
-        cout << "P" << p.pid << "  " << p.start_time << " | " << p.process_time << "\n";
+
 
         if(p.process_time == 0) {
             ex_queue.pop();
-            cout << "--------------\n";
+            cout << "P" << p.pid << "  ";
         }
     }
 }
@@ -48,8 +47,7 @@ int main(int argc, char const *argv[]) {
     map<int, vector<Process> > to_execute;
     priority_queue<Process, std::vector<Process>, CompareProcess> process_queue;
     queue<Process> execute_queue;
-    int i;
-    int n;
+    int i, n;
     unsigned int major_time;
 
     while(true) {
@@ -64,11 +62,9 @@ int main(int argc, char const *argv[]) {
         cout.flush();
 
         for (i = 0;i < n; i++) {
-            cout << "DEBOA" << endl;
             Process p;
             p.pid = i + 1;
             cin >> p.start_time >> p.process_time;
-            cout << p.start_time << " | " << p.process_time << endl;
 
             if (p.start_time > major_time) major_time = p.start_time;
 
@@ -77,41 +73,38 @@ int main(int argc, char const *argv[]) {
         }
 
         cout.flush();
-        cout << "ISSO NAO FAZ SENTIDO!!" << endl << endl;
 
-        int time = 0;
+        unsigned int time = 0;
 
         while(time <= major_time) {
-            cout << "At time: " << time << ":  ";
 
             if (to_execute[time].empty()) {
                 time++;
                 execute_step(execute_queue);
                 if (execute_queue.empty()) {
-                    execute_queue.push(process_queue.top());
-                    process_queue.pop();
+                	if (!process_queue.empty()) {
+                		execute_queue.push(process_queue.top());
+                		process_queue.pop();
+                	}
                 }
-                cout << endl;
                 continue;
             }
             vector<Process> v = to_execute[time];
 
             for (int i = 0; i < v.size(); i++) {
-                Process p = v[i];
-                //cout << "P" << p.pid << "  " << p.start_time << " | " << p.process_time << "\n";
-
                 process_queue.push(v[i]);
             }
 
             execute_step(execute_queue);
 
             if (execute_queue.empty()) {
-                execute_queue.push(process_queue.top());
-                process_queue.pop();
+            	if (!process_queue.empty()) {
+					execute_queue.push(process_queue.top());
+					process_queue.pop();
+				}
             }
 
             time++;
-            cout << endl;
         }
 
         while(!process_queue.empty()) {
@@ -119,10 +112,8 @@ int main(int argc, char const *argv[]) {
             process_queue.pop();
         }
 
-        //cout << "\n\nESTOU AQUI\n\n";
 
         while(!execute_queue.empty()) {
-            cout << "At time: " << time << "  :";
             execute_step(execute_queue);
 
             time++;
