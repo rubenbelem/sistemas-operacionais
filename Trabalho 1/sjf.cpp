e6521cd457ffeb31012f8f134790675f1489b357
Rúben Belém <rubenbelem@gmail.com>
2017-03-27 21:52:34 -0400

    Sjf.cpp atualizado.

--- Trabalho 1/sjf.cpp
+++ Trabalho 1/sjf.cpp
@@ -5,121 +5,202 @@
 
 using namespace std;
 
+enum class ProcessState : int {
+	loaded,
+	ready,
+	running,
+	suspended,
+	finished
+};
+
 struct Process {
     unsigned int pid;
+    unsigned int arrival_time;
     unsigned int start_time;
-    unsigned int process_time;
+    unsigned int instructions;
     unsigned int wait_time;
     unsigned int end_time;
+    ProcessState state;
 
     Process() {
         pid = 0;
         start_time = 0;
-        process_time = 0;
+        instructions = 0;
         wait_time = 0;
         end_time = 0;
+        state = ProcessState::loaded;
     }
 };
 
 class CompareProcess {
     public:
         bool operator() (Process p1, Process p2) {
-            return p1.process_time > p2.process_time;
+            return p1.instructions > p2.instructions;
         }
 };
 
 class Scheduler {
-    
+	private:
+		map<int, vector<Process> > to_execute;
+		priority_queue<Process, std::vector<Process>, CompareProcess> waiting_queue;
+		queue<Process> execute_queue;
+		vector<Process> execution_order;
+
+		unsigned int cnt_time, greater_time;
+		double average_runtime;
+		double average_wait_time;
+
+	public:
+		Scheduler() {
+			cnt_time = 0;
+			greater_time = 0;
+			average_runtime = 0.0;
+			average_wait_time = 0.0;
+		}
+
+		bool execute_step() {
+		    if (!execute_queue.empty()) {
+		        Process &p = execute_queue.front();
+
+		        if (p.state == ProcessState::ready) {
+		        	p.start_time = cnt_time;
+		        	p.wait_time = p.start_time - p.arrival_time;
+
+		        	p.state = ProcessState::running;
+		        }
+
+		        p.instructions--;
+
+		        if(p.instructions == 0) {
+		        	p.end_time = cnt_time;
+		        	p.state = ProcessState::finished;
+
+		        	execution_order.push_back(p);
+		            execute_queue.pop();
+		        }
+
+		        return true;
+		    }
+
+		    return false;
+		}
+
+		void insert_process(Process& p) {
+			if (p.start_time > greater_time) greater_time = p.start_time;
+
+			p.arrival_time = cnt_time;
+			p.state = ProcessState::ready;
+
+			to_execute[p.start_time].push_back(p);
+
+		}
+
+		void clear() {
+			while(!execute_queue.empty()) execute_queue.pop();
+			while(!waiting_queue.empty()) waiting_queue.pop();
+
+			execution_order.clear();
+			to_execute.clear();
+
+			cnt_time = 0;
+			average_runtime = 0.0;
+			average_wait_time = 0.0;
+			greater_time = 0;
+		}
+
+		void run_all() {
+			while(cnt_time <= greater_time) {
+				if (to_execute[cnt_time].empty()) {
+					cnt_time++;
+
+					execute_step();
+
+					if (execute_queue.empty()) {
+						if (!waiting_queue.empty()) {
+							execute_queue.push(waiting_queue.top());
+							waiting_queue.pop();
+						}
+					}
+					continue;
+				}
+
+				vector<Process> v = to_execute[cnt_time];
+
+				for (int i = 0; i < v.size(); i++) {
+					waiting_queue.push(v[i]);
+				}
+
+				execute_step();
+
+				if (execute_queue.empty()) {
+					if (!waiting_queue.empty()) {
+						execute_queue.push(waiting_queue.top());
+						waiting_queue.pop();
+					}
+				}
+
+				cnt_time++;
+			}
+
+			while(!waiting_queue.empty()) {
+				execute_queue.push(waiting_queue.top());
+				waiting_queue.pop();
+			}
+
+			while(!execute_queue.empty()) {
+				execute_step();
+				cnt_time++;
+			}
+		}
+
+		void print_statistics() {
+			int n = execution_order.size();
+			for (int i = 0; i < n; i++) {
+				average_wait_time += execution_order[i].wait_time;
+			}
+
+			average_wait_time /= (double) n;
+
+			cout << "Tempo medio de espera: " << average_wait_time << "s" << endl;
+
+			for (int i = 0; i < n; i++) {
+				cout << "P" << execution_order[i].pid << " ";
+				execution_order.pop_back();
+			}
+		}
+
+		double test() {
+			return average_runtime;
+		}
 };
 
-void execute_step(queue<Process>& ex_queue) {
-    if (!ex_queue.empty()) {
-        Process &p = ex_queue.front();
-        p.process_time--;
-
-        if(p.process_time == 0) {
-            ex_queue.pop();
-            cout << "P" << p.pid << "  ";
-        }
-    }
-}
-
 int main(int argc, char const *argv[]) {
-    std::vector<Process> process_list;
-    map<int, vector<Process> > to_execute;
-    priority_queue<Process, std::vector<Process>, CompareProcess> process_queue;
-    queue<Process> execute_queue;
-    int i, n;
-    unsigned int major_time;
+    int i, n, test = 1;
+    Scheduler scheduler;
 
     while(true) {
         cin >> n;
 
         if (n == 0) break;
 
-        major_time = 0;
-
-        cout << n << endl;
-
-        cout.flush();
-
         for (i = 0;i < n; i++) {
             Process p;
             p.pid = i + 1;
-            cin >> p.start_time >> p.process_time;
+            cin >> p.start_time >> p.instructions;
 
-            if (p.start_time > major_time) major_time = p.start_time;
-
-            to_execute[p.start_time].push_back(p);
-
+            scheduler.insert_process(p);
         }
 
-        cout.flush();
+        scheduler.run_all();
 
-        unsigned int time = 0;
+        cout << "Teste " << test << endl;
 
-        while(time <= major_time) {
+        scheduler.print_statistics();
+        scheduler.clear();
 
-            if (to_execute[time].empty()) {
-                time++;
-                execute_step(execute_queue);
-                if (execute_queue.empty()) {
-                	if (!process_queue.empty()) {
-                		execute_queue.push(process_queue.top());
-                		process_queue.pop();
-                	}
-                }
-                continue;
-            }
-            vector<Process> v = to_execute[time];
+        cout << "\n\n";
 
-            for (int i = 0; i < v.size(); i++) {
-                process_queue.push(v[i]);
-            }
-
-            execute_step(execute_queue);
-
-            if (execute_queue.empty()) {
-            	if (!process_queue.empty()) {
-					execute_queue.push(process_queue.top());
-					process_queue.pop();
-				}
-            }
-
-            time++;
-        }
-
-        while(!process_queue.empty()) {
-            execute_queue.push(process_queue.top());
-            process_queue.pop();
-        }
-
-
-        while(!execute_queue.empty()) {
-            execute_step(execute_queue);
-
-            time++;
-        }
+        test++;
     }
 
     return 0;
